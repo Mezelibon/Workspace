747e81fc4e795af85cbfd28026c850ac59ffd118
Mezelibon <mezelibon@gmail.com>
2023-04-02 18:51:05 +0300

    test2

---
+++ Courses/Course-1/code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    // printf("Hello World!");
+
+    /*
+    
+                DATA TYPES
+
+        int 
+        bool
+        char
+        float
+        double   
+    */
+
+    
+
+
+    return 0;
+}
